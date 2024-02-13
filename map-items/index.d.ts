fafacb115b95b7e0b449f82f4a0d9ca805ba8a24
Maxwell Sultan <maxsultankent@gmail.com>
2024-02-12 22:36:44 -0700

    Updates

--- map-items/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { TrashIcon } from './lib/trash-icon';
-export { ContentMap } from './lib/content-map';
-export type { AddToPracticePlanArgs } from './lib/content-map';
--- map-items/lib/trash-icon.d.ts
+++
@@ -1,5 +0,0 @@
-type TrashIconProps = {
-    className: string;
-};
-export declare const TrashIcon: ({ className }: TrashIconProps) => import("react/jsx-runtime").JSX.Element;
-export {};
